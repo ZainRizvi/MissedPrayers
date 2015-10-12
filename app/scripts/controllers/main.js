52f1216d7b712e721705f32b6840ec14eb43445a
Zain Rizvi <ZainRizvi@users.noreply.github.com>
2015-10-11 23:09:12 -0700

    Initial commit

---
+++ app/scripts/controllers/main.js
@@ -0,0 +1,17 @@
+'use strict';
+
+/**
+ * @ngdoc function
+ * @name missedPrayersApp.controller:MainCtrl
+ * @description
+ * # MainCtrl
+ * Controller of the missedPrayersApp
+ */
+angular.module('missedPrayersApp')
+  .controller('MainCtrl', function () {
+    this.awesomeThings = [
+      'HTML5 Boilerplate',
+      'AngularJS',
+      'Karma'
+    ];
+  });
